2492aa437dee4a271d51c7ba35fa861ee5eb2e34
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2019-02-02 03:53:47 +0000

    Connecting to jdbc

--- src/Sparkjob.py
+++ src/Sparkjob.py
@@ -1,37 +1,49 @@
 import sys
+import pyspark
 from pyspark.sql import SparkSession
-# $example off:init_session$
-
-# $example on:schema_inferring$
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import Row
-# $example off:schema_inferring$
 
-# $example on:programmatic_schema$
-# Import data types
+from pyspark.sql import SQLContext
+
 from pyspark.sql.types import *
-# $example off:programmatic_schema$
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def basic_df_example(spark):
-    # $example on:create_df$
-    # spark is an existing SparkSession
-    df = spark.read.format("csv").option("header","true").option("delimiter",",").load("s3n://creditcardtransaction/trans.csv")
-    # Displays the content of the DataFrame to stdout
-    df.select("Name").show()
 
+    
 
 
 if __name__ == "__main__":
     # $example on:init_session$
+    
     spark = SparkSession \
         .builder \
         .appName("Python Spark SQL") \
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
     # $example off:init_session$
-
-    basic_df_example(spark)
+    df = spark.read.format("csv").option("header","true").option("delimiter",",").load("s3n://creditcardtransaction/trans.csv")
+    df.show()
     
+    card_info =  df.select("PAN","Type","Name","PIN","CVV")
+    df.printSchema()
+    user_info = df.select("SSN","Name","Email","Birthday","Company","Guarantor")
+    guarantor_info = df.select("Guarantor","G-SSN","Name","SSN","PAN")
+    trans_info = df.select("PAN","Transacton Number","Latitude")
+    buy_info = df.select("Name","Date","Transacton Number","Merchant","ConsumType")
+    user_address_info = df.select("Name","SSN","City","Postal","Region","Country")
+#     Displays the content of the DataFrame to stdout
+    sdf_props = {'user':'root','password':'Dapiyanzi123','driver':'com.mysql.jdbc.Driver'}
+    user_address_info.write.jdbc(
+        url='jdbc:mysql://localhost/card_db',
+        table='user_address_info',
+        mode='append',
+        properties = sdf_props
+    )
+
     spark.stop()
+    
+    
     
