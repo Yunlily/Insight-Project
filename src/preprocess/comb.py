6c4d66b55c551e346ab800ed6bbb458db36449ca
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2019-02-02 21:21:02 +0000

    Combine all csv into one

---
+++ src/preprocess/comb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+  
+import os
+import glob
+import pandas as pd
+os.chdir("data/")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#export to csv
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
